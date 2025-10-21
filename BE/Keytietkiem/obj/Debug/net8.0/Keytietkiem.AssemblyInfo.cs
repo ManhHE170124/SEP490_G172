33d165fdae258dfd01e655b0087acd3cf3c90259
ManhLDHE170124 <manhldhe170124@fpt.edu.vn>
2025-10-21 23:13:36 +0700

    modify auditlog

--- BE/Keytietkiem/obj/Debug/net8.0/Keytietkiem.AssemblyInfo.cs
+++ BE/Keytietkiem/obj/Debug/net8.0/Keytietkiem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Keytietkiem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cfd4501e61dda1956933ece6b0db713ea1dbe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448b452cba622d16fd345df09f7c30f5ad31e9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Keytietkiem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Keytietkiem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
